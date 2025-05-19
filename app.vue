0a9e43df1b15aef34a6060f5c3a3e55b676ea03d
Florent Gallois <flgallois@gmail.com>
2025-05-19 10:25:45 +0700

    feature - change tailwindcss theme with local storage

--- app.vue
+++ app.vue
@@ -1,15 +1,61 @@
 <script setup lang="ts">
 useHead({
   htmlAttrs: {
-    "data-theme": "fantasy",
+    "data-theme": "light",
+    class: "light",
   },
 });
+onMounted(() => {
+  const selectedTheme = localStorage.getItem("theme");
+  console.log("LocalStorage value:", selectedTheme);
+  if (selectedTheme) {
+    useHead({
+      bodyAttrs: {
+        "data-theme": selectedTheme,
+        class: selectedTheme,
+      },
+    });
+  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    console.log(
+      "window",
+      window.matchMedia("(prefers-color-scheme: dark)").matches,
+    );
+    useHead({
+      bodyAttrs: {
+        "data-theme": "light",
+        class: "light",
+      },
+    });
+  }
+});
+// document.body.classList.add('light')
+// const selectedTheme = nuxtStorage.localStorage.getItem("theme");
+// if (selectedTheme)
+//   useHead({
+//     bodyAttrs: {
+//       "data-theme": selectedTheme,
+//       class: selectedTheme,
+//     },
+//   });
+// else if (window.matchMedia("(prefers-color-scheme: dark)").matches)
+//   useHead({
+//     bodyAttrs: {
+//       "data-theme": "dark",
+//       class: "dark",
+//     },
+//   });
 </script>
 
 <template>
   <div class="h-full">
-    <NavBar />
-    <NuxtPage />
-    <!-- <Footer/> -->
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+@custom-variant light (&:where([data-theme=light], [data-theme=light] *));
+/*@custom-variant dark (&:where(.light, .light *)); */
+/* @custom-variant dark (&:where([data-theme=dark], [data-theme=dark] *)); */
+</style>
