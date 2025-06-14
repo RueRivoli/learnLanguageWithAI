248260e95b303044377a2c88c71b36a81bd2e46f
Florent Gallois <flgallois@gmail.com>
2025-06-14 22:41:03 +0700

    content - created syllabus file to store data related to different languages

---
+++ utils/syllabus.ts
@@ -0,0 +1,62 @@
+import turkeyFlag from "~/assets/img/turkish.png";
+import franceFlag from "~/assets/img/french.png";
+import spanishFlag from "~/assets/img/spanish.png";
+import italianFlag from "~/assets/img/italian.png";
+import japaneseFlag from "~/assets/img/japanese.png";
+import russianFlag from "~/assets/img/russian.png";
+
+export const languages = [
+  { code: "tr", label: "Turkish", imgSrc: turkeyFlag },
+  { code: "fr", label: "French", imgSrc: franceFlag },
+  { code: "es", label: "Spanish", imgSrc: spanishFlag },
+  { code: "it", label: "Italian", imgSrc: italianFlag },
+  { code: "rs", label: "Russian", imgSrc: russianFlag },
+  { code: "jp", label: "Japanese", imgSrc: japaneseFlag },
+];
+
+export const rulesNames = {
+  tr: [
+    { title: "Ünlü Uyumu", titleEn: "Vocal harmony" },
+    { title: "Ünsüz Uyumu", titleEn: "Consonant harmony" },
+    { title: "İlgeçler", titleEn: "Postpositions" },
+    { title: "Eylem belirteçleri", titleEn: "Verbal modifiers" },
+    { title: "gün (ki...)", titleEn: "The day that ..." },
+    { title: "Dilek kipi", titleEn: "Optative mood" },
+    { title: "Istek kipi", titleEn: "sin" },
+  ],
+  fr: [
+    { title: "Présent indicatif", titleEn: "Present tense" },
+    { title: "Groupe nominale", titleEn: "Noun compound" },
+    { title: "Adjectif qualificatif", titleEn: "Adjective" },
+    { title: "Accents", titleEn: "accents" },
+    { title: "Impératif", titleEn: "Imperativ" },
+  ],
+  es: [
+    { title: "Ukzegjkegz", titleEn: "gzeklkgez" },
+    { title: "egezgezkzl", titleEn: "gjkge" },
+    { title: "hgzeegz;", titleEn: "fez" },
+    { title: "ekzl", titleEn: "fezfezefz" },
+    { title: "ekzl", titleEn: "fezfezfez" },
+  ],
+  it: [
+    { title: "Ukzegjkegz", titleEn: "gzeklkgez" },
+    { title: "egezgezkzl", titleEn: "gjkge" },
+    { title: "hgzeegz;", titleEn: "fez" },
+    { title: "ekzl", titleEn: "fezfezefz" },
+    { title: "ekzl", titleEn: "fezfezfez" },
+  ],
+  jp: [
+    { title: "Ukzegjkegz", titleEn: "gzeklkgez" },
+    { title: "egezgezkzl", titleEn: "gjkge" },
+    { title: "hgzeegz;", titleEn: "fez" },
+    { title: "ekzl", titleEn: "fezfezefz" },
+    { title: "ekzl", titleEn: "fezfezfez" },
+  ],
+  rs: [
+    { title: "Ukzegjkegz", titleEn: "gzeklkgez" },
+    { title: "egezgezkzl", titleEn: "gjkge" },
+    { title: "hgzeegz;", titleEn: "fez" },
+    { title: "ekzl", titleEn: "fezfezefz" },
+    { title: "ekzl", titleEn: "fezfezfez" },
+  ],
+};
