0fc76a5c1eb82f59ab7e2e813a5ac2215f07df3c
Florent Gallois <flgallois@gmail.com>
2025-06-14 21:41:06 +0700

    feature - created a breadcrumbs components
    - display only text for the moment
    - TODO: add links and used it in different pages

---
+++ components/Layout/Breadcrumbs.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    firstSection?: string;
+    secondSection?: string;
+    thirdSection?: string;
+  }>(),
+  {
+    firstSection: null,
+    secondSection: null,
+    thirdSection: null,
+  },
+);
+</script>
+
+<template>
+  <div class="breadcrumbs text-sm">
+    <ul>
+      <li v-if="props.firstSection">
+        <a>{{ props.firstSection }}</a>
+      </li>
+      <li v-if="props.secondSection">
+        <a>{{ props.secondSection }}</a>
+      </li>
+      <li v-if="props.thirdSection">{{ props.thirdSection }}</li>
+    </ul>
+  </div>
+</template>
